b693e0e432452aa8c0a9505a55bc214295b2ee0a
Abrahamlink <whoozzywoo@gmail.com>
2023-11-21 16:51:23 +0300

    Added echo function

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ dp = Dispatcher()
 # Этот хэндлер будет срабатывать на команду "/start"
 @dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
-    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+    await message.answer(f'Привет, {message.from_user.first_name}!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
@@ -42,6 +42,13 @@ async def cmd_cat(message: Message):
             cat_json = await response.json()
             cat = cat_json[0]['url']
             await message.answer_photo(cat)
+
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
 
 
 # Запуск процесса поллинга новых апдейтов
