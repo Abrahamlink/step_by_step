e083d27ec6e639a1a3f60f0c960f6b9d99844046
Abrahamlink <whoozzywoo@gmail.com>
2023-11-22 06:29:24 +0300

    Added game "Guess the number" and auxiliary file "core.py" with logic of game implemented using class

---
+++ core.py
@@ -0,0 +1,129 @@
+import random
+import json
+import asyncio
+
+
+async def get_stats(user_id: int = -1, filename: str = 'addition/stats.json'):
+    with open(filename, 'r', encoding='utf-8') as file:
+        try:
+            players_stats = json.load(file)
+            if user_id == -1:
+                return players_stats
+            return players_stats[f'{user_id}']
+        except Exception as e:
+            return {}
+
+
+async def set_stats(user_id: int, data: dict, filename: str = 'addition/stats.json'):
+    with open(filename, 'w+', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+        file.close()
+        print(f'Stats was saved to {filename} successfully!')
+        return data[f'{user_id}']
+
+
+class Play(object):
+    def __init__(self, user_id, user_stats: dict = dict):
+        self.user_id = user_id
+        if user_stats != {}:
+            self.in_game = user_stats.get('in_game')
+            self.number = user_stats.get('number')
+            self.attempts = user_stats.get('attempts')
+            self.total_games = user_stats.get('total_games')
+            self.wins = user_stats.get('wins')
+        else:
+            self.in_game = False
+            self.number = random.randint(1, 100)
+            self.attempts = 5
+            self.total_games = 0
+            self.wins = 0
+
+    async def stats(self):
+        return {f'{self.user_id}': {
+            'in_game': self.in_game,
+            'number': self.number,
+            'attempts': self.attempts,
+            'total_games': self.total_games,
+            'wins': self.wins
+        }}
+
+    async def save_stats(self, filename: str = 'addition/stats.json'):
+        data = await get_stats(filename=filename)
+        if data == {}:
+            data = await self.stats()
+            print(data, 'if data == {}')
+            user_data = await set_stats(user_id=self.user_id, data=data, filename=filename)
+        else:
+            local_stats = await self.stats()
+            data[f'{self.user_id}'] = local_stats[f'{self.user_id}']
+            print(data, 'else')
+            user_data = await set_stats(user_id=self.user_id, data=data, filename=filename)
+        return user_data
+
+    async def _win(self):
+        self.in_game = False
+        self.total_games += 1
+        self.wins += 1
+        self.attempts = 5
+
+        await self.save_stats()
+        print("Game won!")
+        return 'won'
+
+    async def _mistake(self):
+        print("Mistake!")
+        self.attempts -= 1
+        if self.attempts == 0:
+            return await self.lose()
+        await self.save_stats()
+        return
+
+    async def lose(self):
+        self.in_game = False
+        self.total_games += 1
+
+        await self.save_stats()
+        print("Game lost...")
+        return 'lose'
+
+    async def play(self):
+        """Start the game"""
+        self.in_game = True
+        self.number = random.randint(1, 100)
+        self.attempts = 5
+        await self.save_stats()
+        print("Game started")
+
+    async def try_number(self, number: int):
+        if self.attempts == 0:
+            return await self.lose()
+        if self.number == number:
+            return await self._win()
+        elif self.number > number:
+            mis = await self._mistake()
+            if mis == 'lose':
+                return await self.lose()
+            print("More")
+            return 'more'
+        elif self.number < number:
+            mis = await self._mistake()
+            if mis == 'lose':
+                return await self.lose()
+            print("Less")
+            return 'less'
+
+    async def cancel(self):
+        self.in_game = False
+        self.attempts = 5
+
+        await self.save_stats()
+        return 'cancel'
+
+
+# d = {'313123':
+#          {'in_game': True,
+#           'number': 313123,
+#           'attempts': 5}
+#      }
+#
+# print(list(d.keys())[0])
--- main.py
+++ main.py
@@ -8,6 +8,16 @@ from config import Token
 
 from aiohttp import ClientSession
 
+from core import Play, get_stats
+
+FILENAME = 'addition/stats.json'
+
+
+async def initialize_play(message):
+    player_stats = await get_stats(user_id=message.from_user.id, filename=FILENAME)
+    player = Play(user_id=message.from_user.id, user_stats=player_stats)
+    return player
+
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -20,16 +30,19 @@ dp = Dispatcher()
 # Этот хэндлер будет срабатывать на команду "/start"
 @dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
-    await message.answer(f'Привет, {message.from_user.first_name}!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+    await message.answer(f'Привет, {message.from_user.first_name}!\n'
+                         f'Меня зовут Эхо-бот!\n'
+                         f'Напиши мне что-нибудь\n'
+                         f'Для того, чтобы получить помощь, напиши команду /help')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
 @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(
-        'Напиши мне что-нибудь и в ответ\n'
+        'Напиши мне что-нибудь, и в ответ '
         'я пришлю тебе твое сообщение\n'
-        'Для того, чтобы получить помощь, напиши команду "/help"'
+        'Также мы можем сыграть в игру "Угадай число" Для этого напиши команду /play'
     )
 
 
@@ -44,16 +57,86 @@ async def cmd_cat(message: Message):
             await message.answer_photo(cat)
 
 
+@dp.message(Command("play"))
+async def cmd_play(message: Message):
+    player = await initialize_play(message=message)
+    if not player.in_game:
+        await player.play()
+        await message.answer(text=f'Мы начали играть! Для отмены напиши /cancel\n'
+                                  f'Я загадал число от 1 до 100! Попробуй его угадать))\n'
+                                  f'У тебя есть {player.attempts} попыток\n\n'
+                                  f'Введи число:\n')
+    else:
+        await message.answer(text=f'Мы уже в игре) Осталось попыток: {player.attempts}\n'
+                                  f'Для отмены напиши /cancel')
+
+
+@dp.message(Command("stats"))
+async def cmd_stats(message: Message):
+    player_stats = await get_stats(user_id=message.from_user.id, filename=FILENAME)
+    if player_stats == {}:
+        await message.answer(text=f'У тебя пока нет статистики!')
+    else:
+        win_percentage = 100 * player_stats["wins"] / player_stats["total_games"]
+        await message.answer(text=f'Статистика:\n'
+                                  f'Сыграно игр: {player_stats["total_games"]}\n'
+                                  f'Победы: {player_stats["wins"]}\n'
+                                  f'Процент побед: {win_percentage:.2f}%')
+    del player_stats
+
+
+@dp.message(Command("cancel"))
+async def cmd_cancel(message: Message):
+    player = await initialize_play(message=message)
+    await player.cancel()
+    await message.answer(text=f'Жаль, что ты уходишь :(\n'
+                              f'Если надумаешь сыграть снова, пиши /play\n'
+                              f'Для статистики, напиши /stats\n')
+    del player
+
+
+@dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
+async def process_numbers_answer(message: Message):
+    player = await initialize_play(message=message)
+    if player.in_game:
+        if player.attempts != 0:
+            try_it = await player.try_number(number=int(message.text))
+            if try_it == 'more':
+                await message.answer(text=f'Загаданное число больше, чем {message.text}, попробуй ввести другое\n'
+                                          f'Осталось попыток: {player.attempts}\n\n'
+                                          f'Введи число:\n')
+            elif try_it == 'less':
+                await message.answer(text=f'Загаданное число меньше, чем {message.text}, попробуй ввести другое\n'
+                                          f'Осталось попыток: {player.attempts}\n\n'
+                                          f'Введи число:\n')
+            elif try_it == 'lose':
+                await message.answer(text=f'Ты проиграл... Загаданное число: {player.number}\n'
+                                          f'Хочешь попробовать снова - введи /play')
+            elif try_it == 'won':
+                await message.answer(text=f'Ты выиграл!!! Мои поздравления, это действительно {player.number}!\n'
+                                          f'Хочешь попробовать снова - введи /play')
+        else:
+            await player.lose()
+            await message.answer(text=f'Ты проиграл... Загаданное число: {player.number}\n'
+                                      f'Хочешь попробовать снова - введи /play')
+    else:
+        await message.answer(text=f'Ты не в игре!\n'
+                                  f'Хочешь сыграть - введи /play')
+
+
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
 @dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    try:
+        await message.reply(text=message.text)
+    except:
+        await message.answer(text="Чё за прикол?!")
 
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, polling_timeout=60)
 
 
 if __name__ == "__main__":
