e72e6dbbe2ba7584725bae3f9f5d2291af90c8ee
Abrahamlink <whoozzywoo@gmail.com>
2023-11-25 04:29:31 +0300

    Added last_activity and user_info fields in Play model. Last activity date refresh function added

--- core.py
+++ core.py
@@ -1,3 +1,4 @@
+import datetime
 import random
 import json
 import asyncio
@@ -26,7 +27,9 @@ class Play(object):
     def __init__(self, user_id, user_stats: dict = dict):
         self.user_id = user_id
         if user_stats != {}:
+            self.user_info = user_stats.get('user_info')
             self.in_game = user_stats.get('in_game')
+            self.last_activity = user_stats.get('last_activity')
             self.number = user_stats.get('number')
             self.attempts = user_stats.get('attempts')
             self.total_games = user_stats.get('total_games')
@@ -34,7 +37,9 @@ class Play(object):
             self.cats_coins = user_stats.get('cats_coins')
             self.cats_pics = user_stats.get('cats_pics')
         else:
+            self.user_info = {}
             self.in_game = False
+            self.last_activity = datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
             self.number = random.randint(1, 100)
             self.attempts = 5
             self.total_games = 0
@@ -42,9 +47,11 @@ class Play(object):
             self.cats_coins = 0
             self.cats_pics = []
 
-    async def stats(self):
+    async def stats(self) -> dict:
         return {f'{self.user_id}': {
+            'user_info': self.user_info,
             'in_game': self.in_game,
+            'last_activity': self.last_activity,
             'number': self.number,
             'attempts': self.attempts,
             'total_games': self.total_games,
@@ -57,14 +64,24 @@ class Play(object):
         data = await get_stats(filename=filename)
         if data == {}:
             data = await self.stats()
-            print(data, 'if data == {}')
             user_data = await set_stats(user_id=self.user_id, data=data, filename=filename)
         else:
             local_stats = await self.stats()
             data[f'{self.user_id}'] = local_stats[f'{self.user_id}']
-            print(data, 'else')
             user_data = await set_stats(user_id=self.user_id, data=data, filename=filename)
         return user_data
+
+    async def update_user_info(self, data_to_update: dict = {}):
+        user_stats = await self.stats()
+        user_data = user_stats[f'{self.user_id}'].get('user_info')
+        self.user_info = {**user_data, **data_to_update}
+        await self.save_stats()
+        print('User data updated successfully!\n')
+
+    async def update_date(self, updating_data: str):
+        self.last_activity = updating_data
+        await self.save_stats()
+        print('Date of last activity updated successfully!\n')
 
     async def _win(self):
         self.in_game = False
@@ -130,12 +147,3 @@ class Play(object):
         self.cats_coins -= 1
         self.cats_pics.append(cat_url)
         await self.save_stats()
-
-
-# d = {'313123':
-#          {'in_game': True,
-#           'number': 313123,
-#           'attempts': 5}
-#      }
-#
-# print(list(d.keys())[0])
